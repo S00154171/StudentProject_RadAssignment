eebb66fa86879fb154e7cc31a9fc1affb662e028
S00154171 <fidelrose25@gmail.com>
2018-02-26 15:21:58 +0000

    Project Creation

---
+++ RadAssignment/RadAssignment/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RadAssignment.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RadAssignment/RadAssignment/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RadAssignment.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
